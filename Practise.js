69cb06a93e1f60d853b1da1769cf5490d0bc3517
Gagan-Gupta <gagan88775500@gmail.com>
2023-10-31 14:31:08 +0530

    DataTypes

--- Practise.js
+++ Practise.js
@@ -9,3 +9,18 @@ var emaill = "gagan@google.com";
 
 
 console.table([name, password, emaill]);
+
+//Some Primitive Data Types
+
+// number => 2 to power 53
+// bigint => It is for very large values
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+console.log(typeof 'hello world');
+
+let var1 = null;
+console.log(typeof var1);
