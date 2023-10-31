3315e042da042f8576652dd0351ab9f655ab1707
Gagan-Gupta <gagan88775500@gmail.com>
2023-10-31 10:59:06 +0530

    Learn When to use let var and const

---
+++ Practise.js
@@ -0,0 +1,11 @@
+//Let Const Var
+
+const password = "Hello!123";
+let name = "Gagan";
+var emaill = "gagan@google.com";
+
+// Do Not Use var
+//let have their value only in their scope.
+
+
+console.table([name, password, emaill]);
