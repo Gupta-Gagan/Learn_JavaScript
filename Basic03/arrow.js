b5b9f682d71b3840d6b684a86269d89cc14f10bc
Gagan-Gupta <gagan88775500@gmail.com>
2023-11-05 14:35:11 +0530

    Arrow Functions

---
+++ Basic03/arrow.js
@@ -0,0 +1,40 @@
+const user = {
+    username: "gagan",
+    price: 999,
+    welcomeMessage: function(){
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage();
+/* 
+    this keyword refers to its local context.
+*/
+user.username = "sam";
+// user.welcomeMessage();
+
+// console.log(this); 
+
+// const chai = function () {
+//     let username = "gagan";
+//     console.log(this.username)
+
+// }
+
+// chai();  
+
+const chai = () => {
+    let username = "gagan";
+    console.log(this);
+}
+
+// chai();
+
+const addtwo = (num1, num2) => { return num1 + num2};
+const addthree = (num1, num2, num3) => num1+num2+num3;
+
+// console.log(addthree(5,4,6));
+
+
