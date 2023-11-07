4648340e26d7c504c6ae5eff4cf8509e7ef2e9ae
Gagan-Gupta <gagan88775500@gmail.com>
2023-11-08 00:30:01 +0530

    Switch Cases

---
+++ ControlFlow04/switch.js
@@ -0,0 +1,16 @@
+const month = "march";
+
+switch (month) {
+    case "jan":
+        console.log("January")
+        break;
+    case "feb":
+        console.log("Feburary");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    default:
+        console.log("Please Enter Valid Input!")
+        break;
+}
