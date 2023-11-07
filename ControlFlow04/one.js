0e9c01147050139b61e69e8feba98249b74a8487
Gagan-Gupta <gagan88775500@gmail.com>
2023-11-08 00:26:56 +0530

    if else

---
+++ ControlFlow04/one.js
@@ -0,0 +1,38 @@
+// if
+
+const isUserloggedIn = true;
+const temp = 41;
+
+// if(temp <= 41){
+//     console.log("Less than 50");
+// }
+// else{
+//     console.log("Temp is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+const score = 200;
+// if (score > 100){
+//     let power ="fly";
+//     console.log(`User Power: ${power}`);
+// }
+
+const balance = 1000;
+// if(balance > 500) console.log("Test"),console.log("Cricket");
+
+const userLoggedIn = true;
+const debitCard = true;
+
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if(userLoggedIn && debitCard && 3==3){
+    console.log(`Allow to buy a course.`)
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged In.");
+}
+
